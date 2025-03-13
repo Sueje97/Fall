0be33a5c834446001cd945804649d55216dd65c2
Sujie1997 <161119412+Sueje97@users.noreply.github.com>
2025-03-13 17:54:43 +0800

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -75,9 +75,9 @@ if st.button("Predict"):
     st.write(advice)
     
     # Calculate SHAP values and display force plot 
-    explainer = shap.Explainer(model)
+    explainer = shap.Explainer(model)  
     shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
-    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True, )    
-    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
     
+    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
+    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
     st.image("shap_force_plot.png")
